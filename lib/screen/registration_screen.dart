281ff291eb500f7c73484addf3801b2740427746
ZayatsDmitry <108630172+ZayatsDmitry@users.noreply.github.com>
2023-12-06 22:57:05 +0300

    Finished registr screen (#1)
    * add hint
    
    * add calendar

--- lib/screen/registration_screen.dart
+++ lib/screen/registration_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_authentication_ui/add_utils.dart';
+import 'package:flutter_authentication_ui/widget/input_field_widget.dart';
+import 'package:intl/intl.dart';
 
 class RegistrationScreen extends StatefulWidget {
   const RegistrationScreen({super.key});
@@ -9,6 +11,11 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController birthDateController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +29,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                 "Jack",
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
-                  fontFamily: "Monsterrat",
+                  fontFamily: "Montserrat",
                   fontSize: 28,
                   color: colorWhite,
                 ),
@@ -36,29 +43,101 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                 "of all trades",
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
-                  fontFamily: "Monsterrat",
+                  fontFamily: "Montserrat",
                   fontSize: 28,
                   color: colorWhite,
                 ),
               ),
             ),
             SizedBox(
-              height: MediaQuery.of(context).size.height * 0.03,
+              height: MediaQuery
+                  .of(context)
+                  .size
+                  .height * 0.03,
             ),
             const Center(
               child: Text(
                 "Please, enter your information",
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
-                  fontFamily: "Monsterrat",
+                  fontFamily: "Montserrat",
                   fontSize: 14,
                   color: colorWhite,
                 ),
               ),
             ),
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-
+            const SizedBox(
+              height: 25,
+            ),
+            InputField(
+              controller: nameController,
+              hintText: "Enter your name",
+              icon: Icons.person,
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            InputField(
+              controller: emailController,
+              hintText: "Enter your email",
+              icon: Icons.email,
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            InputField(
+              controller: passwordController,
+              hintText: "Enter your password",
+              icon: Icons.password,
+              obscureText: true,
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 20,
+                right: 20,
+              ),
+              child: TextFormField(
+                style: const TextStyle(
+                  color: colorWhite,
+                  fontFamily: "Montserrat",
+                ),
+                textAlign: TextAlign.center,
+                controller: birthDateController,
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.calendar_month,
+                    color: colorWhite,
+                    size: 25,
+                  ),
+                  hintText: "Enter your birthday",
+                  hintStyle: TextStyle(
+                    color: colorGrey,
+                    fontSize: 12,
+                    fontFamily: "Montserrat",
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: colorWhite),
+                  ),
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(color: colorWhite),
+                  ),
+                ),
+                onTap: () async {
+                  DateTime date = DateTime(1900);
+                  FocusScope.of(context).requestFocus(FocusNode());
+                  date = (await showDatePicker(context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(1900),
+                      lastDate: DateTime(2100)))!;
+                  String dateFormatter = date.toIso8601String();
+                  DateTime dt = DateTime.parse(dateFormatter);
+                  var formatter = DateFormat("dd-MMMM-yyyy");
+                  birthDateController.text = formatter.format(dt);
+                },
+              ),
             ),
           ],
         ),
